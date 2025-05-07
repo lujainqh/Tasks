07f3fcdb8377fec83ad010c28fa40342802f08ca
lujainqh <Lujain.qh1@gmail.com>
2025-05-07 21:19:38 +0300

    adding main

---
+++ IdeaProjects/Task2/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // نتاكد ان فيه 2 باراميتر
+        if (args.length < 2) {
+            System.out.println("Please enter student name and score.");
+            return;
+        }
+        String name = args[0];
+        // عشان لو اليوزر دخل الدرجة بالاحرف احوله للانتجر
+        int score = Integer.parseInt(args[1]);
+        String grade;
+        // نحدد القريد حسب الدرجه
+        if (score >= 90) {
+            grade = "A";
+        } else if (score >= 80) {
+            grade = "B";
+        } else if (score >= 70) {
+            grade = "C";
+        } else if (score >= 60) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+        System.out.println("Student " + name + " your grade: " + grade);
+    }
+}
